6891d7652059788bb8fb71ad048a5698116d2547
RMGonzalo <gonzalo363rm@gmail.com>
2024-04-19 16:30:29 -0300

    fix: (user): (selector): default values

--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -4,7 +4,7 @@ export const selectUser = createSelector(
     (state) => state.User,
     (user) => ({
         user: user?.get('id') ? user : null,
-        loading: user.get('loading'),
-        error: user.get('error')
+        loading: user?.get('loading') ?? false,
+        error: user?.get('error') ?? null
     }),
 );
